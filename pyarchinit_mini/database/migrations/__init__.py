1ddb671be42a0afc0b4f7ff9183dd1d060518506
enzo <enzo.ccc@gmail.com>
2026-07-02 22:11:53 +0200

    feat(struttura): concurrency migration adds sync columns to struttura_table

--- pyarchinit_mini/database/migrations/__init__.py
+++ pyarchinit_mini/database/migrations/__init__.py
@@ -184,7 +184,7 @@ class DatabaseMigrations:
                 'periodizzazione_table', 'media_table', 'media_thumb_table',
                 'documentation_table',
                 'pyarchinit_thesaurus_sigle', 'thesaurus_field', 'thesaurus_category',
-                'users', 'tomba_table',
+                'users', 'tomba_table', 'struttura_table',
             ]
 
             concurrency_columns = [
